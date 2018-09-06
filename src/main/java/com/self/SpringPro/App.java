77d4a22623806dae1757af7f3a39798f414c89fe
unknown <poornadaatla@gmail.com>
2018-09-06 22:32:46 +0530

    First Commit

---
+++ src/main/java/com/self/SpringPro/App.java
@@ -0,0 +1,13 @@
+package com.self.SpringPro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
